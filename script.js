10968a961068aaf65b2839561cd1fd86100aa31f
Akshar Bisht <bisht.akshar@gmail.com>
2026-08-19 16:40:40 +0530

    Added functions to generate computer choice and take user input

---
+++ script.js
@@ -0,0 +1,12 @@
+let humanScore = 0
+let computerScore = 0
+//function to generate and return a random number 0 <= number >= 2
+function getComputerChoice() {
+    return Math.floor(Math.random() * 3)
+}
+
+//function to take user choice and return it
+function getHumanChoice() {
+    let userChoice = prompt("Please enter your choice")
+}
+
